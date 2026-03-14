f81d083e42db26dfe68ff5f86b3ffeef93844d55
BBT <bbt@Amans-MacBook-Air.local>
2026-03-14 10:36:27 +0530

    add

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,18 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hrms-lite';
-
-
-// export const connectDatabase = async (): Promise<void> => {
-//   try {
-//     await mongoose.connect(MONGODB_URI);
-//     console.log('✅ MongoDB connected successfully');
-//   } catch (error) {
-//     console.error('❌ MongoDB connection error:', error);
-//     process.exit(1);
-//   }
-// };
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
